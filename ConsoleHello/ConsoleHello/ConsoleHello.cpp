c3f72e29210522c90ad4704f101b852370f87a54
shejun6 <1390979524@qq.com>
2019-08-18 03:04:20 +0800

    位移计算

--- ConsoleHello/ConsoleHello/ConsoleHello.cpp
+++ ConsoleHello/ConsoleHello/ConsoleHello.cpp
@@ -16,25 +16,3 @@ int main()
     cout << (x >> n) << endl;
     system("pause");
 }
-
-//   3. 使用输出窗口查看生成输出和其他消息
-// ConsoleHello.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-	cout << "佘军的项目" << endl;
-	short n = 256;
-	int x = 1;
-    //std::cout << "Hello111 World!\n"; 
-	//cout << "佘军的项目" << endl;
-	cout << (x >> n) << endl;
-	system("pause");
-}
-
-//   3. 使用输出窗口查看生成输出和其他消息
